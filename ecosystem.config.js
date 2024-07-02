bd1cd71ee932c433397349d2ef4586b20ec7d5e4
maraiza20026 <maraiza20026@gmail.com>
2024-07-02 15:41:50 -0600

    combined

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [
+      {
+        name: 'vite-app',
+        cwd: './frontend',  // Change to frontend directory
+        script: 'npm',
+        args: 'run dev',
+        env: {
+          NODE_ENV: 'development',
+          PORT: 3000
+        }
+      },
+      {
+        name: 'backend',
+        cwd: './backend',  // Change to backend directory
+        script: 'server.js',
+        env: {
+          NODE_ENV: 'production',
+          PORT: 5000
+        }
+      }
+    ]
+  };
+  
