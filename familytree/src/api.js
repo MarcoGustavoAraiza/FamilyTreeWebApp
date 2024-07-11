7c467a362a7cee75b39b475f6d26377f1fbf00c5
maraiza20026 <maraiza20026@gmail.com>
2024-07-10 22:51:39 -0600

    small change

--- familytree/src/api.js
+++ familytree/src/api.js
@@ -1,12 +1,12 @@
 export const fetchAllFamilyNames = async () => {
-    const response = await fetch('http://3.129.127.64/all-family-names');
+    const response = await fetch('http://3.129.127.64/api/all-family-names');
     // const response = await fetch('http://localhost:5000/all-family-names');
     const data = await response.json();
     return data;
 };
 
 export const fetchPeople = async () => {
-    const response = await fetch('http://3.129.127.64/people');
+    const response = await fetch('http://3.129.127.64/api/people');
     // const response = await fetch('http://localhost:5000/people');
     const data = await response.json();
     return data;
